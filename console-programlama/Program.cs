94fbb922a17698f3a6e55790583ab945208e1e52
SÜHA BERK KUKUK <shbkukuk@gmail.com>
2021-12-22 01:35:04 +0300

    first commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adinizi Giriniz:");
+            string name=Console.ReadLine();
+            Console.WriteLine("Soyadinizi Giriniz:");
+            string soyad = Console.ReadLine();
+
+            Console.WriteLine("Merhaba"+ name + " " + soyad );
+
+        }
+    }
+}
