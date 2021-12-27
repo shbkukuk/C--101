234aacd90bb1a0c0ab3e1c7820dbbf7ceb25c935
SÜHA BERK KUKUK <shbkukuk@gmail.com>
2021-12-27 15:48:26 +0300

    switch-case added

--- if_else/Program.cs
+++ if_else/Program.cs
@@ -20,6 +20,14 @@ namespace if_else
             Console.WriteLine(sonuc);
             Console.WriteLine(mod);
 
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+            if (N%2 == 0 && N>=2&&N<=5 || N>20)
+                Console.WriteLine("Not Weird");
+            else if(N%2 == 0 &&  N>=6&&N<=20)
+                Console.WriteLine("Weird");
+            else 
+                Console.WriteLine("Weird");
+
         }
     }
 }
---
+++ swicth_case/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace swicth_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            switch (month)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    Console.WriteLine("Kış ayındasınız");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("bahar SEn hoşgeldin");
+                    break;
+                case 6 :
+                case 7 :
+                case 8 :
+                    Console.WriteLine("YAZIN GÜNEŞİ ALNIMA VURDU");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("SONbahar mısın Yoksa ilk AÇan mı ");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
