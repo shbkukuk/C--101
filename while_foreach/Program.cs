737805f32bb43627813f0b6bbd8bbbc54dc1c706
SÜHA BERK KUKUK <shbkukuk@gmail.com>
2021-12-27 18:02:37 +0300

    while_foreach added

---
+++ while_foreach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 1;
+            while (sayac <= 5)
+            {
+                Console.WriteLine(sayac);
+                sayac++;
+            }
+            
+            int[] sayiDizisi = {2,4,5,1,2,4};
+            int toplam = 0 ;
+            
+            foreach(int i in SayiDizisi)
+            {
+                toplam += i ;
+            }
+        }
+    }
+}
