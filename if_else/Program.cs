536ef15bebb6af285e86bbe2a90ca65406fcc216
SÜHA BERK KUKUK <shbkukuk@gmail.com>
2021-12-27 14:38:24 +0300

    if_else added

---
+++ if_else/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if(time>=6 && time<11)
+                Console.WriteLine("Günaydın Saat :"+Convert.ToString(time));
+            else if(time<18)
+                Console.WriteLine("İyi Günler Saat " + Convert.ToString(time));
+            else
+                Console.WriteLine("İyi Akşamlar Saat " +Convert.ToString(time));
+
+            string sonuc = time<18 ? "İyi günler" : "İyi Akşamlar"; // ? ifadesi öncesi koşul eğer doğru ise "?" yanındaki ilk argüman çalışır.
+            //Yanlış ise ":" ifadesinin sağ tarafındaki argüman çalışır.  (Ternary If)
+            string mod = time>=6 && time<11 ? "Günaydın" : time<18 ? " İyi günler" : "iyi akşamlar";
+            Console.WriteLine(sonuc);
+            Console.WriteLine(mod);
+
+        }
+    }
+}
---
+++ karar_yapıları/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace karar_yapıları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
